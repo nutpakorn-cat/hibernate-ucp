5cf94d583fe2aa9c3550edf52ca659dde9558864
nutterrocker <nutter@admin.in.th>
2014-03-22 11:23:57 +0700

    เพิ่มหน้าแรกลงไป
    เพิ่มหน้าแรก

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,26 +1,22 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
+/*
+ * ไฟล์ Welcome เป็นไฟล์หลัก
+ */
 class Welcome extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
+    public function index()
+    {
+        if($this->session->userdata("login") == TRUE) //ถ้าล็อกอินอยู่แล้ว
+        {
+            
+        }
+        else //ถ้ายังไม่ได้ล็อกอิน
+        {
+            $data['result'] = $this->db->get_where("tb_setting",array(
+                "server_id" => "1"
+            ));
+            $this->load->view("index",$data);
+        }
+    }
 }
 
 /* End of file welcome.php */
---
+++ application/views/theme/user/header.php
@@ -0,0 +1,13 @@
+<?php
+$row = $result->row();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?=$row->server_name?></title>
+    </head>
+    <body>
+
+    </body>
+</html>
