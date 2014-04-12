bf09a0310a9f69c9bb62b71b6d8f6f54a41b0892
nutterrocker <nutter@admin.in.th>
2014-04-12 15:12:22 +0700

    เพิ่มระบบ Reset พาสแอดมิน

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -34,6 +34,83 @@ class admin extends CI_Controller {
             $this->load->view("admin/auth",$data);
         }
     }
+    public function edit()
+    {
+        if($this->session->userdata("login_admin") == TRUE)
+        {
+            if($this->input->post("v1",TRUE) != NULL && $this->input->post("v2",TRUE) != NULL && $this->input->post("v3",TRUE) != NULL && $this->input->post("v4",TRUE) != NULL && $this->input->post("v5",TRUE) != NULL)
+            {
+                $ip = $this->input->post("v1",TRUE);
+                $port = $this->input->post("v2",TRUE);
+                $rcon = $this->input->post("v3",TRUE);
+                $rcon_port = $this->input->post("v4",TRUE);
+                $this->db->where(array(
+                    "server_id" => "1"
+                ));
+                $this->db->update("tb_setting",array(
+                    "server_ip" => "{$this->input->post("v1",TRUE)}",
+                    "server_port" => "{$this->input->post("v2",TRUE)}",
+                    "server_rcon" => "{$this->input->post("v3",TRUE)}",
+                    "server_rcon_port" => "{$this->input->post("v4",TRUE)}"
+                ));
+                redirect("admin");
+            }
+            else if($this->input->post("v6",TRUE) != NULL && $this->input->post("v7",TRUE) != NULL)
+            {
+                $this->db->where(array(
+                    "server_id" => "1"
+                ));
+                $this->db->update("tb_setting",array(
+                    "server_name" => "{$this->input->post("v6",TRUE)}"
+                ));
+                redirect("admin");
+            }
+            else
+            {
+                redirect("admin");
+            }
+        }
+        else
+        {
+            redirect("welcome");
+        }
+    }
+    public function reset()
+    {
+        if($this->session->userdata("login_admin") == TRUE)
+        {
+            if($this->input->post("v1",TRUE) != NULL && $this->input->post("v2",TRUE) != NULL && $this->input->post("v3",TRUE) != NULL && $this->input->post("v4",TRUE) != NULL && $this->input->post("v2",TRUE) == $this->input->post("v3",TRUE))
+            {
+                $username = $this->input->post("v1",TRUE);
+                $password = $this->input->post("v2",TRUE); 
+                $salt = substr(md5(rand(0,10000)),0,16);
+                $realpassword = $salt . md5($salt . $password);
+                $this->db->where(array(
+                    "server_id" => "1"
+                ));
+                
+                $this->db->update("tb_setting",array(
+                    "server_admin" => "$username",
+                    "server_pass" => "$realpassword"
+                ));
+                $this->session->set_userdata(array(
+                    "username_admin" => "$username"
+                ));
+                redirect("admin");
+            }
+            else
+            {
+                $data['result'] = $this->db->get_where("tb_setting",array(
+                    "server_id" => "1"
+                ));
+                $this->load->view("admin/reset",$data);
+            }
+        }
+        else 
+        {
+            redirect("welcome");
+        }
+    }
     public function auth()
     {
         if($this->session->userdata("login_admin") == TRUE)
--- application/views/admin/control.php
+++ application/views/admin/control.php
@@ -28,6 +28,7 @@ $this->load->view("theme/admin/header");
                         "class" => "btn btn-lg btn-success btn-block",
                         "value" => "เพิ่ม"
                     ));
+                    echo form_close();
                     ?>
                     <hr>
                     <?=anchor("plugin/reload","โหลดการทำงานใหม่",array("class" => "btn btn-lg btn-block btn-primary"))?>
@@ -60,6 +61,79 @@ $this->load->view("theme/admin/header");
                         ?>
                       </tbody>
                     </table>
+                </div>
+            </div>
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">จัดการ IP,Port,Rcon,Rcon Port</h3>
+                </div>
+                <div class="panel-body">
+                    <?php
+                    $row_result = $result->row();
+                    echo form_open("admin/edit");
+                    echo form_input(array(
+                        "name" => "v1",
+                        "class" => "form-control",
+                        "value" => "$row_result->server_ip",
+                        "placeholder" => "IP เซิฟเวอร์",
+                        "required" => ""
+                    ));
+                    echo "<p></p>";
+                    echo form_input(array(
+                        "name" => "v2",
+                        "class" => "form-control",
+                        "value" => "$row_result->server_port",
+                        "placeholder" => "PORT เซิฟเวอร์",
+                        "required" => ""
+                    ));
+                    echo "<p></p>";
+                    echo form_input(array(
+                        "name" => "v3",
+                        "class" => "form-control",
+                        "value" => "$row_result->server_rcon",
+                        "placeholder" => "Rcon Password",
+                        "required" => ""
+                    ));
+                    echo "<p></p>";
+                    echo form_input(array(
+                        "name" => "v4",
+                        "class" => "form-control",
+                        "value" => "$row_result->server_rcon_port",
+                        "placeholder" => "Rcon Port เซิฟเวอร์",
+                        "required" => ""
+                    ));
+                    echo "<p></p>";
+                    echo form_submit(array(
+                        "name" => "v5",
+                        "class" => "btn btn-lg btn-block btn-danger",
+                        "value" => "เปลี่ยน IP,PORT,RCON,RCON PORT"
+                    ));
+                    echo form_close();
+                    ?>
+                </div>
+            </div>
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">แก้ไขชื่อเว็บไซต์</h3>
+                </div>
+                <div class="panel-body">
+                    <?php
+                    echo form_open("admin/edit");
+                    echo form_input(array(
+                        "name" => "v6",
+                        "class" => "form-control",
+                        "placeholder" => "ชื่อเว็บไซต์",
+                        "value" => "$row_result->server_name",
+                        "required" => ""
+                    ));
+                    echo "<p></p>";
+                    echo form_submit(array(
+                        "name" => "v7",
+                        "class" => "btn btn-lg btn-block btn-warning",
+                        "value" => "เปลี่ยนชื่อเว็บไซต์"
+                    ));
+                    echo form_close();
+                    ?>
                 </div>
             </div>
             <!-- [ Plugins Control ] -->
